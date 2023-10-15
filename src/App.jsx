28eb62b28578e54401d6a9e2fce2365a509eb209
KonnyGuo <guokonny@gmail.com>
2023-10-14 23:37:17 -0400

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <h2>Advanced React</h2>
     </div>
   );
 }
 
 export default App;
+
+// import Starter from './tutorial/1-useState/starter/1-error-example';
+// import Final from './tutorial/1-useState/final/1-error-example';
+// function App() {
+//   return (
+//     <div className='container'>
+//       <Starter />
+//       <Final />
+//     </div>
+//   );
+// }
+
+// export default App;
