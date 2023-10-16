783bb4ef4617528537095b1ff098cd7d1d8ba3aa
KonnyGuo <guokonny@gmail.com>
2023-10-15 22:42:58 -0400

    useState

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,6 +1,10 @@
+// logic does not work in react because rerender is not getting trigger -> need to set up state value
+// use state hook
+
 const ErrorExample = () => {
   let count = 0;
   const handleClick = () => {
+    console.log(count);
     count++;
   };
 
