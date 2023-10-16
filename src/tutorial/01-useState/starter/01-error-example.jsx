14a9180037d5a1d8da70c378a1382f1e155c4839
KonnyGuo <guokonny@gmail.com>
2023-10-15 22:39:01 -0400

    01-01

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,17 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0;
+  const handleClick = () => {
+    count++;
+  };
+
+  return (
+    <div>
+      <h2> {count} </h2>
+      <button className="btn" type="submit" onClick={handleClick}>
+        addCount
+      </button>
+    </div>
+  );
 };
 
 export default ErrorExample;
