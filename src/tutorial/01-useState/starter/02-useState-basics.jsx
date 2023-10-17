79c45dbb10631b1c1b0374e63dac8aea2a2b58bc
KonnyGuo <guokonny@gmail.com>
2023-10-16 21:55:53 -0400

    use state

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+//can also type React.useState
+//need to invoke function
+
 const UseStateBasics = () => {
+  console.log(useState());
   return <h2>useState basics</h2>;
 };
 
