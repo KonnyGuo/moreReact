91d5c8da2b4259df02a038a89d02bbb4c5ced532
KonnyGuo <guokonny@gmail.com>
2023-10-17 22:30:16 -0400

    useState

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,25 @@
+import { data, people } from "../../../data";
+import { useState } from "react";
+
+// hook rules
+// Component must be captialized, keyword "use", don't call hooks conditionally, invoke instead function or component body,
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [data, useData] = useState(data);
+  // const [people, usePeople] = useState(people);
+  const loopData = () => {
+    for (let i = 0; i < data.length; i++) {
+      console.log(`${i}: ${data[i].name}`);
+    }
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={loopData}>
+        click here
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
