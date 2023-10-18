6d537d164e61441d41b2daff41c1e88bbd10faa0
KonnyGuo <guokonny@gmail.com>
2023-10-17 22:50:21 -0400

    using in array

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,22 +1,48 @@
-import { data, people } from "../../../data";
+import { data } from "../../../data";
 import { useState } from "react";
 
 // hook rules
 // Component must be captialized, keyword "use", don't call hooks conditionally, invoke instead function or component body,
 
 const UseStateArray = () => {
-  const [data, useData] = useState(data);
-  // const [people, usePeople] = useState(people);
-  const loopData = () => {
-    for (let i = 0; i < data.length; i++) {
-      console.log(`${i}: ${data[i].name}`);
-    }
+  // const [data, useData] = useState(data);
+  const [people, setPeople] = useState(data);
+  const removeItem = (id) => {
+    const newPeople = people.filter((person) => {
+      if (person.id !== id) {
+        return person;
+      }
+    });
+    setPeople(newPeople);
+  };
+
+  const removeAll = () => {
+    //make people into empty array
+    setPeople([]);
   };
 
   return (
     <div>
-      <button type="button" onClick={loopData}>
-        click here
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4> {name} </h4>
+            <button
+              onClick={() => {
+                removeItem(id);
+              }}
+              type="button"
+            >
+              {" "}
+              remove{" "}
+            </button>
+          </div>
+        );
+      })}
+
+      <button onClick={removeAll} type="button" style={{ marginTop: "2rem" }}>
+        remove all
       </button>
     </div>
   );
