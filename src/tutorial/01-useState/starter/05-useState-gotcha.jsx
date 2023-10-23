5aea656938d68ca967805b5ca8b921b92029ba97
KonnyGuo <guokonny@gmail.com>
2023-10-22 22:04:01 -0400

    useState complete

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 
+// access latest value at all time using function with states. This is important for some other functions that may rely on needing the latest value from the current function.
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
 
   const handleClick = () => {
-    setValue(value + 1);
+    // setValue(value + 1);
+
+    setValue((currentState) => {
+      const newState = currentState + 1;
+      console.log(newState);
+      return newState;
+    });
+    // console.log(value);
   };
   return (
     <div>
