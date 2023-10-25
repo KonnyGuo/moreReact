a4f1227aaaab5fe37e86d5e21b55eab1b72ef99a
KonnyGuo <guokonny@gmail.com>
2023-10-24 21:54:29 -0400

    multiple effs

--- src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
+
+// useEffect for subscription, fetching data, messing with dom. Works outside of the component
+// 2 parameters that it accepts, callback function and dependency array (optional). Callback function runs after each render by default but no promises can be return (no async await). The dependency array if empty then it only runs on inital render
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
   const sayHello = () => {
-    console.log('hello there');
+    console.log("hello there");
   };
 
   sayHello();
 
+  useEffect(() => {
+    console.log("hello from use eff");
+  }, []);
+
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
