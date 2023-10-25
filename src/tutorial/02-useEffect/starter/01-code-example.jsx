d928eb63e0c51c6a8c9658b5ac9a142848f3b8d2
KonnyGuo <guokonny@gmail.com>
2023-10-24 21:46:38 -0400

    edits

--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const CodeExample = () => {
   const [value, setValue] = useState(0);
 
+  const sayHello = () => {
+    console.log("hello there");
+    // setValue(value + 1);
+  };
+  // this function will run each time CodeExample is rerendered
+  // be careful with trigger infinite loop like with comment in function
+  sayHello();
+
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
