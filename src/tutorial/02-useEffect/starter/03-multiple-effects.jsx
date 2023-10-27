7dc708f5ef3465d2994990f2cbe280ac6072e4fb
KonnyGuo <guokonny@gmail.com>
2023-10-26 21:51:51 -0400

    use eff

--- src/tutorial/02-useEffect/starter/03-multiple-effects.jsx
+++ src/tutorial/02-useEffect/starter/03-multiple-effects.jsx
@@ -1,12 +1,27 @@
 import { useState, useEffect } from "react";
 
+// can set up multiple use effect
+// useEffect(() => {
+//   effect function
+// }, [deps]);
+// Empty array - []:
+// This runs the effect only once on mount and unmount.
+// It won't re-run on state changes or re-renders.
+// Dependency array - [dep1, dep2]:
+// This runs the effect whenever dep1 or dep2 change.
+// Great for when you only want to re-run when certain data updates.
+// So in summary:
+
+// [] - Runs on mount/unmount only
+// [deps] - Runs on mount and when deps change
+
 const MultipleEffects = () => {
   const [value, setValue] = useState(0);
   const [secondValue, setSecondValue] = useState(0);
 
-  // useEffect(() => {
-  //   console.log('hello from first useEffect');
-  // }, []);
+  useEffect(() => {
+    console.log("hello from first useEffect");
+  }, [value, secondValue]);
 
   useEffect(() => {
     console.log("hello from second useEffect");
