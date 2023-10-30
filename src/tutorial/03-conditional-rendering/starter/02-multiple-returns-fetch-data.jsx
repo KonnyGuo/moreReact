b42fd55d13887701a4689601043fed6837539697
KonnyGuo <guokonny@gmail.com>
2023-10-29 22:49:24 -0400

    multiple returns

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -3,18 +3,43 @@ const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const response = await fetch(url);
         const user = await response.json();
-        console.log(user);
+        setUser(user);
       } catch (err) {
+        setIsError(true);
         console.log(err);
       }
+      setIsLoading(false);
     };
+    fetchUser();
   }, []);
 
-  return <h2>Fetch Data </h2>;
+  if (isLoading) {
+    return <h2> is loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2> error detected </h2>;
+  }
+
+  return (
+    <div>
+      <img
+        style={{ width: "150px", borderRadius: "25px" }}
+        src={user.avatar_url}
+        alt={user.name}
+      />
+      <h2> {user.name} </h2>
+      <h4> works at {user.company} </h4>
+      <p> {user.bio} </p>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
