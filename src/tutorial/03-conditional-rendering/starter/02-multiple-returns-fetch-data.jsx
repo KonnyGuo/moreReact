e38b2cda12e730b657e241bb14c28e00eedc8f1a
KonnyGuo <guokonny@gmail.com>
2023-10-29 22:38:08 -0400

    multiple returns

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,20 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(url);
+        const user = await response.json();
+        console.log(user);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  }, []);
+
   return <h2>Fetch Data </h2>;
 };
 export default MultipleReturnsFetchData;
