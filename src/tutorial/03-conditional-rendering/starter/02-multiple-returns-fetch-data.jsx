0908b206b7c8e9b126920e6d57d11d69bcda4848
KonnyGuo <guokonny@gmail.com>
2023-10-31 23:21:15 -0400

    edit

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -11,6 +11,11 @@ const MultipleReturnsFetchData = () => {
     const fetchUser = async () => {
       try {
         const response = await fetch(url);
+        if (!response.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
         const user = await response.json();
         setUser(user);
       } catch (err) {
