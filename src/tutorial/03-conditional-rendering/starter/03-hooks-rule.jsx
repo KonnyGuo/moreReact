fb96e315e5e89897c4bd42b2f6826802ae94f286
KonnyGuo <guokonny@gmail.com>
2023-11-11 23:24:06 -0500

    edit

--- src/tutorial/03-conditional-rendering/starter/03-hooks-rule.jsx
+++ src/tutorial/03-conditional-rendering/starter/03-hooks-rule.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 // falsly values in js include 0, false, empty string, null, underfine, nan
 // truthy values includes objects and arrays
 // short circuit evaluation using && ||
+// using &&, returns first operand if falsy. Return second operand if its falsy and first one is truthy ex: const x = 0, const y = 1. x && y returns 0. y && x returns 0
+// using ||, returns first operand if truthy. Return second operand first one is falsy
 
 const Example = () => {
   const [condition, setCondition] = useState(true);
