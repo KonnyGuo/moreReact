c9d130f616233eea65a6b41f0381e071e876e017
KonnyGuo <guokonny@gmail.com>
2023-11-20 22:14:43 -0500

    06

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [showAlert, setAlert] = useState(false);
+
+  const toggleAlert = () => {
+    if (showAlert) {
+      setAlert(false);
+      return;
+    }
+
+    setAlert(true);
+  };
+
+  return (
+    <div>
+      <button className="" onClick={toggleAlert}>
+        {" "}
+        toggle button{" "}
+      </button>
+      <Alert />
+    </div>
+  );
+};
+
+const Alert = () => {
+  return (
+    <div className="alert alert-danger">
+      <h2>hello world</h2>
+    </div>
+  );
 };
 
 export default ToggleChallenge;
