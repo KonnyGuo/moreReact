983d0d635719f159ba229c75cb1a3930357243de
KonnyGuo <guokonny@gmail.com>
2023-11-27 22:08:33 -0500

    02 05

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 const CleanupFunction = () => {
   const [toggle, setToggle] = useState(false);
@@ -7,11 +8,18 @@ const CleanupFunction = () => {
       <button className="btn" onClick={() => setToggle(!toggle)}>
         toggle component
       </button>
+      {/* if toggle is true in jsx, then return random component */}
+      {toggle && <RandomComponent />}
     </div>
   );
 };
 
 const RandomComponent = () => {
+  // in any component we can set up useEffect, 2 arguments function and dependency array
+  // if you are displaying useEffect conditionally then it can render multiple times
+  useEffect(() => {
+    console.log("how many times will this show up");
+  }, []);
   return <h1> hello world </h1>;
 };
 
