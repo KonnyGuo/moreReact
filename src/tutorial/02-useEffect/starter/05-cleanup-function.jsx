19e2f74e90713d3e6f05f1aa06dac14039fcb285
KonnyGuo <guokonny@gmail.com>
2023-12-18 21:13:28 -0500

    use eff done

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -15,21 +15,36 @@ const CleanupFunction = () => {
 };
 
 const RandomComponent = () => {
-  // in any component we can set up useEffect, 2 arguments function and dependency array
-  // if you are displaying useEffect conditionally then it can render multiple times
   useEffect(() => {
-    console.log("how many times will this show up");
-    // need to set up an id for clear interval otherwise the setInterval will keep running
-    const intID = setInterval(() => {
-      console.log("from interval");
-    }, 1000);
-    // cleanup function below
-    return () => {
-      clearInterval(intID);
-      console.log("cleanup");
+    const someFunc = () => {
+      // any logic
     };
+    window.addEventListener("scroll", someFunc);
+    // inspect elements event listener to see scroll
+
+    // cleanup function so that scroll event does not keep happening
+    return () => window.addEventListener("scroll", someFunc);
   }, []);
   return <h1> hello world </h1>;
 };
+
+// const RandomComponent = () => {
+//   // in any component we can set up useEffect, 2 arguments function and dependency array
+//   // if you are displaying useEffect conditionally then it can render multiple times
+//   useEffect(() => {
+//     console.log("how many times will this show up");
+//     // need to set up an id for clear interval otherwise the setInterval will keep running
+//     const intID = setInterval(() => {
+//       console.log("from interval");
+//     }, 1000);
+//     // cleanup function below
+//     // ex of subscribing and unsubscribing
+//     return () => {
+//       clearInterval(intID);
+//       console.log("cleanup");
+//     };
+//   }, []);
+//   return <h1> hello world </h1>;
+// };
 
 export default CleanupFunction;
